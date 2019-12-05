92360a0dcd04b7d506942035350e13a4d4c58246
Davinder Kumar <davinderkumar8166@gmail.com>
2019-12-05 22:36:12 +0530

    Test

--- learning/src/Person/Person.js
+++ learning/src/Person/Person.js
@@ -7,8 +7,7 @@ const person = (props) => {
             <p>{props.children}</p>
             <input type="text" onChange={props.change} defaultValue={props.name} />
         </div>
-    )
-        ;
+    );
 
 }
 export default person
